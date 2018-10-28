2c67b77bf9e3fa51a85112694278232a29ea3c35
DennisWeyrauch <dennisweyrauch93@gmail.com>
2018-10-28 18:45:25 +0100

    Add files via upload

---
+++ Dennis/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radio1 = new System.Windows.Forms.RadioButton();
+            this.radio2 = new System.Windows.Forms.RadioButton();
+            this.radio3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 0; // Auto-added
+            this.button1.Text = "TestInputBox";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // radio1
+            // 
+            this.radio1.Location = new System.Drawing.Point(180, 20);
+            this.radio1.Name = "radio1";
+            this.radio1.Size = new System.Drawing.Size(67, 17);
+            this.radio1.TabIndex = 5;
+            this.radio1.Text = "Radio 0";
+            this.radio1.CheckedChanged += new System.EventHandler(this.radio1_CheckedChanged);
+            // 
+            // radio2
+            // 
+            this.radio2.Location = new System.Drawing.Point(180, 50);
+            this.radio2.Name = "radio2";
+            this.radio2.Size = new System.Drawing.Size(67, 17);
+            this.radio2.TabIndex = 6;
+            this.radio2.Text = "Radio 1";
+            this.radio2.CheckedChanged += new System.EventHandler(this.radio1_CheckedChanged);
+            // 
+            // radio3
+            // 
+            this.radio3.Location = new System.Drawing.Point(180, 80);
+            this.radio3.Name = "radio3";
+            this.radio3.Size = new System.Drawing.Size(67, 17);
+            this.radio3.TabIndex = 7;
+            this.radio3.Text = "Radio 2";
+            this.radio3.CheckedChanged += new System.EventHandler(this.radio1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);// 36, 17
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(80, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);//97, 17
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "CheckBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 212);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radio3);
+            this.Controls.Add(this.radio2);
+            this.Controls.Add(this.radio1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(200, 200);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radio1;
+        private System.Windows.Forms.RadioButton radio2;
+        private System.Windows.Forms.RadioButton radio3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+

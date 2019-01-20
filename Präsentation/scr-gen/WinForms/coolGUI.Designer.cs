e5136395cb7db13cc8c6ef278af54ca3c230b80d
Dennis <dennisweyrauch93@gmail.com>
2019-01-20 21:25:14 +0100

    New Binary

--- Präsentation/scr-gen/WinForms/coolGUI.Designer.cs
+++ Präsentation/scr-gen/WinForms/coolGUI.Designer.cs
@@ -45,80 +45,72 @@ namespace ns_coolGUI {
 			// 
 			// myButton1
 			// 
-			this.myButton1.AutoSize = true;
 			this.myButton1.Name = "myButton1";
 			this.myButton1.Location = new System.Drawing.Point(10, 180);
-			this.myButton1.Size = new System.Drawing.Size(0, 0);
+			this.myButton1.AutoSize = true;
 			this.myButton1.TabIndex = 0;
 			this.myButton1.Text = "Button 1";
 			this.Controls.Add(this.myButton1);
 			// 
 			// myButton2
 			// 
-			this.myButton2.AutoSize = true;
 			this.myButton2.Name = "myButton2";
 			this.myButton2.Location = new System.Drawing.Point(80, 180);
-			this.myButton2.Size = new System.Drawing.Size(0, 0);
+			this.myButton2.AutoSize = true;
 			this.myButton2.TabIndex = 1;
 			this.myButton2.Text = "Button 2";
 			this.Controls.Add(this.myButton2);
 			// 
 			// myLabel1
 			// 
-			this.myLabel1.AutoSize = true;
 			this.myLabel1.Name = "myLabel1";
 			this.myLabel1.Location = new System.Drawing.Point(10, 10);
-			this.myLabel1.Size = new System.Drawing.Size(0, 0);
+			this.myLabel1.AutoSize = true;
 			this.myLabel1.TabIndex = 2;
 			this.myLabel1.Text = "Das ist eine Test GUI";
 			this.Controls.Add(this.myLabel1);
 			// 
 			// myCheckBox
 			// 
-			this.myCheckBox.AutoSize = true;
 			this.myCheckBox.Name = "myCheckBox";
 			this.myCheckBox.Location = new System.Drawing.Point(10, 50);
-			this.myCheckBox.Size = new System.Drawing.Size(0, 0);
+			this.myCheckBox.AutoSize = true;
 			this.myCheckBox.TabIndex = 3;
 			this.myCheckBox.Text = "Check Mich";
 			this.Controls.Add(this.myCheckBox);
 			// 
 			// myInput1
 			// 
-			this.myInput1.AutoSize = true;
 			this.myInput1.Name = "myInput1";
 			this.myInput1.Location = new System.Drawing.Point(10, 80);
-			this.myInput1.Size = new System.Drawing.Size(0, 0);
+			this.myInput1.AutoSize = true;
 			this.myInput1.TabIndex = 4;
 			this.myInput1.Text = "Schreib was";
 			this.Controls.Add(this.myInput1);
 			// 
 			// myRB1
 			// 
-			this.myRB1.AutoSize = true;
 			this.myRB1.Name = "myRB1";
 			this.myRB1.Location = new System.Drawing.Point(10, 110);
-			this.myRB1.Size = new System.Drawing.Size(0, 0);
+			this.myRB1.AutoSize = true;
 			this.myRB1.TabIndex = 5;
 			this.myRB1.Text = "1";
 			this.Controls.Add(this.myRB1);
 			// 
 			// myRB2
 			// 
-			this.myRB2.AutoSize = true;
 			this.myRB2.Name = "myRB2";
 			this.myRB2.Location = new System.Drawing.Point(10, 130);
-			this.myRB2.Size = new System.Drawing.Size(0, 0);
+			this.myRB2.AutoSize = true;
 			this.myRB2.TabIndex = 6;
 			this.myRB2.Text = "2";
 			this.Controls.Add(this.myRB2);
 			// 
 			// myRB3
 			// 
-			this.myRB3.AutoSize = true;
 			this.myRB3.Name = "myRB3";
 			this.myRB3.Location = new System.Drawing.Point(10, 150);
-			this.myRB3.Size = new System.Drawing.Size(0, 0);
+			this.myRB3.AutoSize = true;
 			this.myRB3.TabIndex = 7;
 			this.myRB3.Text = "3";
 			this.Controls.Add(this.myRB3);
--- Präsentation/scr-gen/WinForms/coolGUI.cs
+++ Präsentation/scr-gen/WinForms/coolGUI.cs
@@ -8,48 +8,48 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace coolGUI {
-    public partial class coolGUI : Form {
-        public coolGUI() {
-            InitializeComponent();
-        }
-        
-        public void addCallbacks(Form 1) {
-        	// this.NAME.click += new System.EventHandler(...);
-			this.myButton1.click += new System.EventHandler(...)
-			this.myButton2.click += new System.EventHandler(...)
-			this.myLabel1.click += new System.EventHandler(...)
-			this.myCheckBox.click += new System.EventHandler(...)
-			this.myInput1.click += new System.EventHandler(...)
-			this.myRB1.click += new System.EventHandler(...)
-			this.myRB2.click += new System.EventHandler(...)
-			this.myRB3.click += new System.EventHandler(...)
-        }
+namespace ns_coolGUI {
+	public partial class coolGUI : Form {
+		public coolGUI() {
+			InitializeComponent();
+		}
 		
-private void myCheckBox_CheckedChanged(object sender, EventArgs e) {
-    var chk = (CheckBox) sender;
-    if (chk.Checked)
-        MessageBox.Show("Checked", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-    else
-        MessageBox.Show("Unchecked", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-}
-private void myInput1_Click(object sender, EventArgs e) {
-    MessageBox.Show("Hello World", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-}
-private void myRB1_CheckedChanged(object sender, EventArgs e) {
-    var rad = (RadioButton) sender;
-    if (!rad.Checked) return;
-    MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-}
-private void myRB2_CheckedChanged(object sender, EventArgs e) {
-    var rad = (RadioButton) sender;
-    if (!rad.Checked) return;
-    MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-}
-private void myRB3_CheckedChanged(object sender, EventArgs e) {
-    var rad = (RadioButton) sender;
-    if (!rad.Checked) return;
-    MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
-}
-    }
+		public void addCallbacks() {
+			this.myButton1.Click += new System.EventHandler(this.myButton1_Click);
+			this.myButton2.Click += new System.EventHandler(this.myButton2_Click);
+			this.myCheckBox.CheckedChanged += new System.EventHandler(this.myCheckBox_CheckedChanged);
+			this.myRB1.CheckedChanged += new System.EventHandler(this.myRB1_CheckedChanged);
+			this.myRB2.CheckedChanged += new System.EventHandler(this.myRB2_CheckedChanged);
+			this.myRB3.CheckedChanged += new System.EventHandler(this.myRB3_CheckedChanged);
+		}
+		
+		private void myButton1_Click(object sender, EventArgs e) {
+			MessageBox.Show("Hello World von Button 1", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		private void myButton2_Click(object sender, EventArgs e) {
+			MessageBox.Show("Hello World von Button 2", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		private void myCheckBox_CheckedChanged(object sender, EventArgs e) {
+			var chk = (CheckBox) sender;
+			if (chk.Checked)
+				MessageBox.Show("Checked", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			else
+				MessageBox.Show("Unchecked", "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		private void myRB1_CheckedChanged(object sender, EventArgs e) {
+			var rad = (RadioButton) sender;
+			if (!rad.Checked) return;
+			MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		private void myRB2_CheckedChanged(object sender, EventArgs e) {
+			var rad = (RadioButton) sender;
+			if (!rad.Checked) return;
+			MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		private void myRB3_CheckedChanged(object sender, EventArgs e) {
+			var rad = (RadioButton) sender;
+			if (!rad.Checked) return;
+			MessageBox.Show(rad.Text, "InputBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+	}
 }
